19c4d93e6ed22fe158e90b920a2830257dccaa63
Manu <manuel.chavez@accenture.com>
2022-07-23 13:14:43 -0500

    including unit tests

---
+++ Ecommerce.Api.Products.Tests/ProductsTest.cs
@@ -0,0 +1,93 @@
+using AutoMapper;
+using Ecommerce.Api.Products.Db;
+using Ecommerce.Api.Products.Profiles;
+using Ecommerce.Api.Products.Providers;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Api.Products.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public async Task GetProdutsReturnAllProducts()
+    {
+        var options = new DbContextOptionsBuilder<ProductsDbContext>()
+                        .UseInMemoryDatabase(nameof(GetProdutsReturnAllProducts)).Options;
+
+        var dbContext = new ProductsDbContext(options);
+        CreateProducts(dbContext);
+
+        var productProfile = new ProductProfile();        
+        var configuration = new MapperConfiguration(cfg => cfg.AddProfile(productProfile));
+        var mapper = new Mapper(configuration);
+
+        var productsProvider = new ProductsProvider(dbContext, null, mapper);
+        
+        var product = await productsProvider.GetProductsAsync();
+
+        Assert.True(product.IsSuccess);
+        Assert.True(product.Products.Any());
+        Assert.Null(product.ErrorMessage);
+
+    }
+
+    [Fact]
+    public async Task GetProdutsReturnProductUsingValidId()
+    {
+        var options = new DbContextOptionsBuilder<ProductsDbContext>()
+                        .UseInMemoryDatabase(nameof(GetProdutsReturnAllProducts)).Options;
+
+        var dbContext = new ProductsDbContext(options);
+        CreateProducts(dbContext);
+
+        var productProfile = new ProductProfile();        
+        var configuration = new MapperConfiguration(cfg => cfg.AddProfile(productProfile));
+        var mapper = new Mapper(configuration);
+
+        var productsProvider = new ProductsProvider(dbContext, null, mapper);
+        
+        var product = await productsProvider.GetProductAsync(1);
+
+        Assert.True(product.IsSuccess);
+        Assert.NotNull(product.Product);
+        Assert.True(product.Product.Id == 1);
+        Assert.Null(product.ErrorMessage);
+    }
+
+    [Fact]
+    public async Task GetProdutsReturnProductUsingInvalidId()
+    {
+        var options = new DbContextOptionsBuilder<ProductsDbContext>()
+                        .UseInMemoryDatabase(nameof(GetProdutsReturnAllProducts)).Options;
+
+        var dbContext = new ProductsDbContext(options);
+        CreateProducts(dbContext);
+
+        var productProfile = new ProductProfile();        
+        var configuration = new MapperConfiguration(cfg => cfg.AddProfile(productProfile));
+        var mapper = new Mapper(configuration);
+
+        var productsProvider = new ProductsProvider(dbContext, null, mapper);
+        
+        var product = await productsProvider.GetProductAsync(-1);
+
+        Assert.False(product.IsSuccess);
+        Assert.Null(product.Product);
+        Assert.NotNull(product.ErrorMessage);
+    }
+
+    private void CreateProducts(ProductsDbContext dbContext)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            dbContext.Products.Add(new Product
+            {
+                Id = i,
+                Name = Guid.NewGuid().ToString(),
+                Inventory = i + 10,
+                Price = (decimal)(i * 3.14)
+            });
+        }      
+        dbContext.SaveChanges();  
+    }
+}
